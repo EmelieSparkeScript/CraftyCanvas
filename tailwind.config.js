5b12a0f643148141f1fd0008f4030b481c7cc4aa
johanssonemelie <emelietheresiah@gmail.com>
2024-01-01 15:15:49 +0100

    added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'carbon-fiber': '#2D2D2D',
+        'dou-lu-green': '#009379',
+        'sunset-orange': '#FF6250',
+        'sango-pink': "#F3AFA8",
+        'bleached-silk': "#F3F3F3",
+        'pixel-cream': "#F7D684",
+      }
+    },
   },
   plugins: [],
 };
